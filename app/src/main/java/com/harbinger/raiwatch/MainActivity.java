704c71299ed7c9c78d9a4192615d0e5a3ca1111e
alanbrilliant <abrilliant2017@gmail.com>
2017-12-29 21:32:38 -0800

    Initial Commit

--- app/src/main/java/com/harbinger/raiwatch/MainActivity.java
+++ app/src/main/java/com/harbinger/raiwatch/MainActivity.java
@@ -1,13 +1,50 @@
 package com.harbinger.raiwatch;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.*;
+
+import org.json.JSONObject;
+
+
+import java.net.*;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HttpURLConnection urlConnection;
+
+        StringBuilder result = new StringBuilder();
+
+
+        try {
+            URL url = new URL("https://api.coinmarketcap.com/v1/ticker/raiblocks/");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            urlConnection.disconnect();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        String resultSTr = result.toString();
+
+
+
     }
+
+
+
 }
